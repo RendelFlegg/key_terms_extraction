4913ade99a0f786477474577801b10a2304f557e
RendelFlegg <rendelflegg@ya.ru>
2022-06-08 18:30:37 +0300

    Completed Stage 1: Implemented XML parsing and tokenization

--- Key Terms Extraction/task/key_terms.py
+++ Key Terms Extraction/task/key_terms.py
@@ -1,1 +1,19 @@
-# Write your code here
+import nltk
+from collections import Counter
+from lxml import etree
+
+dictionary = {}
+xml_path = 'news.xml'
+
+tree = etree.parse(xml_path)
+root = tree.getroot()
+
+for n in range(len(root[0])):
+    head, text = root[0][n][0].text, root[0][n][1].text
+    tokens = nltk.tokenize.word_tokenize(text.lower())
+    dictionary[head] = Counter(sorted(tokens, reverse=True))
+
+for key in dictionary:
+    print(f'{key}:')
+    print(*[n[0] for n in dictionary[key].most_common(5)])
+    print()
