43d856c84f3cc19475e5a811d36a1f45a0fa9b72
RendelFlegg <rendelflegg@ya.ru>
2022-06-09 16:43:03 +0300

    Completed Stage 2: Implemented lemmatization and tokens filtering

--- Key Terms Extraction/task/key_terms.py
+++ Key Terms Extraction/task/key_terms.py
@@ -1,17 +1,34 @@
+import string
 import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 from collections import Counter
 from lxml import etree
 
+
+def process_news(tag):
+    head, text = tag[0].text, tag[1].text
+    tokens = get_tokens(text)
+    dictionary[head] = Counter(sorted(tokens, reverse=True))
+
+
+def get_tokens(text):
+    tokens = nltk.tokenize.word_tokenize(text.lower())
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    tokens = [token for token in tokens if token not in list(string.punctuation) + stopwords.words('english')]
+    return tokens
+
+
+lemmatizer = WordNetLemmatizer()
 dictionary = {}
 xml_path = 'news.xml'
 
 tree = etree.parse(xml_path)
 root = tree.getroot()
+corpus = root[0]
 
-for n in range(len(root[0])):
-    head, text = root[0][n][0].text, root[0][n][1].text
-    tokens = nltk.tokenize.word_tokenize(text.lower())
-    dictionary[head] = Counter(sorted(tokens, reverse=True))
+for n in range(len(corpus)):
+    process_news(corpus[n])
 
 for key in dictionary:
     print(f'{key}:')
