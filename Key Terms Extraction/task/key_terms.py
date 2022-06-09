059f86c0c9ccdeb2f04c5ddcc6a71db5a7474847
RendelFlegg <rendelflegg@ya.ru>
2022-06-09 16:43:03 +0300

    Completed Stage 3: Implemented POS-tagging

--- Key Terms Extraction/task/key_terms.py
+++ Key Terms Extraction/task/key_terms.py
@@ -1,17 +1,51 @@
+import string
 import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 from collections import Counter
 from lxml import etree
 
+
+def process_news(tag):
+    head, text = tag[0].text, tag[1].text
+    tokens = get_tokens(text)
+    dictionary[head] = Counter(sorted(tokens, reverse=True))
+
+
+def get_tokens(text):
+    tokens = nltk.tokenize.word_tokenize(text.lower())
+    tokens = get_lemmas(tokens)
+    tokens = eliminate_meaningless(tokens)
+    tokens = get_nouns(tokens)
+    return tokens
+
+
+def get_lemmas(tokens):
+    return [lemmatizer.lemmatize(token) for token in tokens]
+
+
+def eliminate_meaningless(tokens):
+    return [token for token in tokens if token not in list(string.punctuation) + stopwords.words('english')]
+
+
+def get_nouns(tokens):
+    nouns = []
+    for token in tokens:
+        if nltk.pos_tag([token])[0][1] == 'NN':
+            nouns.append(token)
+    return nouns
+
+
+lemmatizer = WordNetLemmatizer()
 dictionary = {}
 xml_path = 'news.xml'
 
 tree = etree.parse(xml_path)
 root = tree.getroot()
+corpus = root[0]
 
-for n in range(len(root[0])):
-    head, text = root[0][n][0].text, root[0][n][1].text
-    tokens = nltk.tokenize.word_tokenize(text.lower())
-    dictionary[head] = Counter(sorted(tokens, reverse=True))
+for n in range(len(corpus)):
+    process_news(corpus[n])
 
 for key in dictionary:
     print(f'{key}:')
