412e99a7ba1c4ceebb31ba71d34db006d478c1e7
RendelFlegg <rendelflegg@ya.ru>
2022-06-14 16:47:25 +0300

    Completed Stage 4: Implemented TF-IDF vectoring

--- Key Terms Extraction/task/key_terms.py
+++ Key Terms Extraction/task/key_terms.py
@@ -2,14 +2,14 @@ import string
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-from collections import Counter
 from lxml import etree
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def process_news(tag):
     head, text = tag[0].text, tag[1].text
     tokens = get_tokens(text)
-    dictionary[head] = Counter(sorted(tokens, reverse=True))
+    dictionary[head] = {'tokens': tokens}
 
 
 def get_tokens(text):
@@ -25,7 +25,7 @@ def get_lemmas(tokens):
 
 
 def eliminate_meaningless(tokens):
-    return [token for token in tokens if token not in list(string.punctuation) + stopwords.words('english')]
+    return [token for token in tokens if token not in list(string.punctuation) + stopwords.words('english')]  # + ['ha', 'wa', 'u', 'a']]
 
 
 def get_nouns(tokens):
@@ -47,7 +47,19 @@ corpus = root[0]
 for n in range(len(corpus)):
     process_news(corpus[n])
 
+news_collection = [' '.join(dictionary[key]['tokens']) for key in dictionary]
+
+vectorizer = TfidfVectorizer(tokenizer=get_tokens)
+tfidf_matrix = vectorizer.fit_transform(news_collection)
+terms = vectorizer.get_feature_names()
+
+for n, key in enumerate(dictionary):
+    dictionary[key]['weights'] = {}
+    for word in dictionary[key]['tokens']:
+        dictionary[key]['weights'][word] = tfidf_matrix[(n, terms.index(word))]
+
 for key in dictionary:
     print(f'{key}:')
-    print(*[n[0] for n in dictionary[key].most_common(5)])
+    res = [val[0] for val in sorted(dictionary[key]['weights'].items(), key=lambda x: (x[1], x[0]), reverse=True)]
+    print(*res[:5])
     print()
